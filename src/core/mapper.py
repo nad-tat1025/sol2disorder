1eef6ebe108e44469139631307e952d93304805e
hanada <nad-tat1025@g.ecc.u-tokyo.ac.jp>
2025-07-12 15:13:18 +0900

    all added

--- src/core/mapper.py
+++ src/core/mapper.py
@@ -14,29 +14,69 @@ class HamiltonianMapper:
         self.analyzer = analyzer
         self.config = config
 
-    def construct_hermitian_hops(self, bravais_vectors: List[BravaisVector]) -> Dict[BravaisVector, np.ndarray]:
-        """ホッピング行列の辞書を構築し、エルミート対称性を保証する。"""
-        final_hop = {}
+    # def construct_hermitian_hops(self, bravais_vectors: List[BravaisVector]) -> Dict[BravaisVector, np.ndarray]:
+    #     """ホッピング行列の辞書を構築し、エルミート対称性を保証する。"""
+    #     final_hop = {}
         
-        if not self.config.get("use_hermitian_symmetrization", True):
-            for R_tuple in bravais_vectors:
-                final_hop[R_tuple] = self._construct_global_block(np.array(R_tuple))
-            return final_hop
+    #     if not self.config.get("use_hermitian_symmetrization", True):
+    #         for R_tuple in bravais_vectors:
+    #             final_hop[R_tuple] = self._construct_global_block(np.array(R_tuple))
+    #         return final_hop
 
-        processed_R = set()
-        for R_tuple in bravais_vectors:
-            if R_tuple in processed_R: continue
+    #     processed_R = set()
+    #     for R_tuple in bravais_vectors:
+    #         if R_tuple in processed_R: continue
             
+    #         H_R = self._construct_global_block(np.array(R_tuple))
+    #         # final_hop[R_tuple] = H_R
+    #         final_hop[R_tuple] = H_R.T.conj() # TEST
+            
+            
+    #         minus_R_tuple = tuple(-r for r in R_tuple)
+    #         if R_tuple != minus_R_tuple:
+    #             # final_hop[minus_R_tuple] = H_R.T.conj()
+    #             final_hop[minus_R_tuple] = H_R # TEST
+            
+    #         processed_R.add(R_tuple)
+    #         processed_R.add(minus_R_tuple)
+        
+    #     if (0, 0, 0) in final_hop:
+    #         H_0 = final_hop[(0, 0, 0)]
+    #         final_hop[(0, 0, 0)] = (H_0 + H_0.T.conj()) / 2.0
+            
+    #     return final_hop
+
+    def construct_hermitian_hops(self, bravais_vectors: List[BravaisVector]) -> Dict[BravaisVector, np.ndarray]:
+        """
+        ホッピング行列の辞書を構築する。
+        ループ構造を統一し、use_hermitian_symmetrizationフラグは計算方法の切り替えにのみ使用する。
+        """
+        final_hop = {}
+        processed_R = set()
+        symmetrize = self.config.get("use_hermitian_symmetrization", True)
+
+        for R_tuple in bravais_vectors:
+            if R_tuple in processed_R:
+                continue
+
+            # H(R)を計算
             H_R = self._construct_global_block(np.array(R_tuple))
             final_hop[R_tuple] = H_R
-            
+
             minus_R_tuple = tuple(-r for r in R_tuple)
             if R_tuple != minus_R_tuple:
-                final_hop[minus_R_tuple] = H_R.T.conj()
+                if symmetrize:
+                    # trueの場合: H(-R) = H(R)† を適用して対称性を保証
+                    final_hop[minus_R_tuple] = H_R.T.conj()
+                else:
+                    # falseの場合: H(-R)も独立して計算する
+                    H_minus_R = self._construct_global_block(np.array(minus_R_tuple))
+                    final_hop[minus_R_tuple] = H_minus_R
             
             processed_R.add(R_tuple)
             processed_R.add(minus_R_tuple)
         
+        # オンサイト項がエルミートになるように保証 (どちらのケースでも実行)
         if (0, 0, 0) in final_hop:
             H_0 = final_hop[(0, 0, 0)]
             final_hop[(0, 0, 0)] = (H_0 + H_0.T.conj()) / 2.0
